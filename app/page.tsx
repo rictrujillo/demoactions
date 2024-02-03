db5c9b86d7b51f53d41942f3e2553d430744721f
Ricardo Trujillo <rictrujillo@gmail.com>
2024-02-02 19:38:57 -0600

    feat: add a paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,10 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <div>
+      <h1>Mi pagina</h1>
+      <p>Esto es un parrafo en la pagina principal</p>
+    </div>
+  );
 }
